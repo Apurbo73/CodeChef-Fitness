34d1956db7556a4cbd8a1ffc9d98c9214d14b354
Apurbo73 <apurbodebnath895@gmail.com>
2025-03-26 12:05:39 +0600

    done

---
+++ problem.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int a;
+    cin >> a;
+    while (a--)
+    {
+        int b;
+        cin >> b;
+        cout << b * 2 * 5 << endl;
+    }
+}
